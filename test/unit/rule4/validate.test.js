9b2e8cfcb2e1ab276fe4b2405e5d8ac29a4ca598
gowpen <33104741+gowpen@users.noreply.github.com>
2017-10-30 15:38:28 -0600

    Refactor rule4 unit tests

--- test/unit/rule4/validate.test.js
+++ test/unit/rule4/validate.test.js
@@ -1,12 +1,14 @@
 import test from 'ava'
 const rule4 = require('../../../lib/rule4')
 
-test('accept unterminated subject', t => {
-  const message = 'Subject'
+function accept (t, message) {
   t.true(rule4.validate(message))
-})
+}
 
-test('reject terminated subject', t => {
-  const message = 'Subject.'
+function reject (t, message) {
   t.false(rule4.validate(message))
-})
+}
+
+test('accept unterminated subject', accept, 'Subject')
+
+test('reject terminated subject', reject, 'Subject.')
