ee72061427d50616fd5cea8bc1a1d27c6b66c3ff
gowpen <33104741+gowpen@users.noreply.github.com>
2017-10-30 01:25:41 -0600

    Add rule6 unit tests

---
+++ test/unit/rule6.test.js
@@ -0,0 +1,16 @@
+import test from 'ava'
+var rule6 = require('../../lib/rule6')
+
+test('accept wrapped body', t => {
+  const line = 'x'.repeat(20)
+  const body = line + '\n' + line
+  const message = 'Subject\n\n' + body
+  t.true(rule6.validate(message))
+})
+
+test('reject unwrapped body', t => {
+  const line = 'x'.repeat(100)
+  const body = line + '\n' + line
+  const message = 'Subject\n\n' + body
+  t.false(rule6.validate(message))
+})
