b0c04f31f8965f4ba9759dae95f519adad289430
gowpen <33104741+gowpen@users.noreply.github.com>
2017-10-30 00:58:58 -0600

    Add rule1 unit tests

---
+++ test/unit/rule1.test.js
@@ -0,0 +1,23 @@
+// Imports
+import test from 'ava'
+var rule1 = require('../../lib/rule1')
+
+test('accept no body', t => {
+  const message = 'Subject line'
+  t.true(rule1.validate(message))
+})
+
+test('accept single line break', t => {
+  const message = 'Subject line\n'
+  t.true(rule1.validate(message))
+})
+
+test('accept double line break', t => {
+  const message = 'Subject line\n\n'
+  t.true(rule1.validate(message))
+})
+
+test('reject unseparated', t => {
+  const message = 'Subject line\nBody'
+  t.false(rule1.validate(message))
+})
