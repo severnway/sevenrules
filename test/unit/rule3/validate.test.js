6d32b690205226cd98261548b1e28a488790efab
gowpen <33104741+gowpen@users.noreply.github.com>
2017-10-30 15:36:45 -0600

    Refactor rule3 unit tests

--- test/unit/rule3/validate.test.js
+++ test/unit/rule3/validate.test.js
@@ -1,12 +1,14 @@
 import test from 'ava'
 const rule3 = require('../../../lib/rule3')
 
-test('accept capitalized subject', t => {
-  const message = 'Subject'
+function accept (t, message) {
   t.true(rule3.validate(message))
-})
+}
 
-test('reject uncapitalized subject', t => {
-  const message = 'subject'
+function reject (t, message) {
   t.false(rule3.validate(message))
-})
+}
+
+test('accept capitalized subject', accept, 'Subject')
+
+test('reject uncapitalized subject', reject, 'subject')
