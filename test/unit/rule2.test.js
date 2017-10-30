f203d9a5c10f2a23bb9a4a0670ae6bad0e3032e8
gowpen <33104741+gowpen@users.noreply.github.com>
2017-10-30 01:09:57 -0600

    Add rule2 unit tests

---
+++ test/unit/rule2.test.js
@@ -0,0 +1,28 @@
+// Imports
+import test from 'ava'
+var rule2 = require('../../lib/rule2')
+
+test('accept under 50 characters', t => {
+  const message = 'X'.repeat(20)
+  t.true(rule2.validate(message))
+})
+
+test('accept 50 characters', t => {
+  const message = 'X'.repeat(50)
+  t.true(rule2.validate(message))
+})
+
+test('accept over 50 under 72 characters', t => {
+  const message = 'X'.repeat(60)
+  t.true(rule2.validate(message))
+})
+
+test('accept 72 characters', t => {
+  const message = 'X'.repeat(72)
+  t.true(rule2.validate(message))
+})
+
+test('reject over 72 characters', t => {
+  const message = 'X'.repeat(73)
+  t.false(rule2.validate(message))
+})
