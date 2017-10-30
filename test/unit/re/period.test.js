e3564377616acaaa9e20a2e7fa435d8a81c74135
gowpen <33104741+gowpen@users.noreply.github.com>
2017-10-30 15:25:06 -0600

    Refactor period unit tests

--- test/unit/re/period.test.js
+++ test/unit/re/period.test.js
@@ -1,12 +1,13 @@
 import test from 'ava'
 const re = require('../../../lib/re')
 
-test('match period', t => {
-  const period = '.'
-  t.not(period.match(re.period), null)
-})
+function match (t, text) {
+  t.not(text.match(re.period), null)
+}
 
-test('miss letter', t => {
-  const letter = 'a'
-  t.is(letter.match(re.period), null)
-})
+function miss (t, text) {
+  t.is(text.match(re.period), null)
+}
+
+test('match period', match, '.')
+test('miss letter', miss, 'a')
