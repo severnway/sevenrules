812cd87e90703eec6df34ca090da4aecf50521ad
gowpen <33104741+gowpen@users.noreply.github.com>
2017-10-30 15:21:10 -0600

    Refactor lineBreak unit tests

--- test/unit/re/lineBreak.test.js
+++ test/unit/re/lineBreak.test.js
@@ -1,22 +1,15 @@
 import test from 'ava'
 const re = require('../../../lib/re')
 
-test('match CR', t => {
-  const CR = '\u{D}'
-  t.not(CR.match(re.lineBreak), null)
-})
+function match (t, text) {
+  t.not(text.match(re.lineBreak), null)
+}
 
-test('match LF', t => {
-  const LF = '\u{A}'
-  t.not(LF.match(re.lineBreak), null)
-})
+function miss (t, text) {
+  t.is(text.match(re.lineBreak), null)
+}
 
-test('match CRLF', t => {
-  const CRLF = '\u{D}\u{A}'
-  t.not(CRLF.match(re.lineBreak), null)
-})
-
-test('miss letter', t => {
-  const letter = 'a'
-  t.is(letter.match(re.lineBreak), null)
-})
+test('match CR', match, '\u{D}')
+test('match LF', match, '\u{A}')
+test('match CRLF', match, '\u{D}\u{A}')
+test('miss letter', miss, 'a')
