68382b768e081e5fe44c08f2b18587eb96909a34
gowpen <33104741+gowpen@users.noreply.github.com>
2017-10-30 13:52:03 -0600

    Structure unit tests

--- test/unit/rule2/validate.test.js
+++ test/unit/rule2/validate.test.js
@@ -1,5 +1,5 @@
 import test from 'ava'
-const rule2 = require('../../lib/rule2')
+const rule2 = require('../../../lib/rule2')
 
 test('accept under 50 characters', t => {
   const message = 'X'.repeat(20)
