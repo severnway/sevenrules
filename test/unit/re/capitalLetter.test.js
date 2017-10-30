1b5c4e35d7d4e0e50982b0aededc9a1e35e3ffd0
gowpen <33104741+gowpen@users.noreply.github.com>
2017-10-30 15:22:54 -0600

    Refactor capitalLetter unit tests

--- test/unit/re/capitalLetter.test.js
+++ test/unit/re/capitalLetter.test.js
@@ -1,12 +1,13 @@
 import test from 'ava'
 const re = require('../../../lib/re')
 
-test('match capital', t => {
-  const capital = 'A'
-  t.not(capital.match(re.capitalLetter), null)
-})
+function match (t, text) {
+  t.not(text.match(re.capitalLetter), null)
+}
 
-test('miss lowercase', t => {
-  const lowercase = 'a'
-  t.is(lowercase.match(re.capitalLetter), null)
-})
+function miss (t, text) {
+  t.is(text.match(re.capitalLetter), null)
+}
+
+test('match capital', match, 'A')
+test('miss lowercase', miss, 'a')
