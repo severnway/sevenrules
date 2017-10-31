0cb79a2591f75af0038fe28206876c18e188d34f
gowpen <33104741+gowpen@users.noreply.github.com>
2017-10-30 23:57:11 -0600

    Implement husky hook

--- bin/husky.js
+++ bin/husky.js
@@ -1,6 +1,37 @@
+#!/usr/bin/env node
+
 /**
  * @file Husky hook to validate with sevenrules.
  * @module sevenruleshusky
  */
 
 'use strict'
+
+// Imports
+var fs = require('fs')
+var sevenrules = require('..')
+
+var messagePath = process.env['GIT_PARAMS']
+if (!messagePath) {
+  throw new Error('Must be run as a husky commit-msg hook')
+}
+var message = fs.readFileSync(messagePath, 'utf8')
+var results = sevenrules.validateAll(message)
+if (!(
+  results[1] &&
+  results[2] &&
+  results[3] &&
+  results[4] &&
+  results[6]
+)) {
+  console.log('')
+  console.log('Valorous brethren, thy commit message hast some problems.')
+  console.log('Please try the following changes:')
+  if (!results[1]) console.log('  ' + sevenrules.rule1.rule)
+  if (!results[2]) console.log('  ' + sevenrules.rule2.rule)
+  if (!results[3]) console.log('  ' + sevenrules.rule3.rule)
+  if (!results[4]) console.log('  ' + sevenrules.rule4.rule)
+  if (!results[6]) console.log('  ' + sevenrules.rule6.rule)
+  console.log('')
+  process.exit(1)
+}
